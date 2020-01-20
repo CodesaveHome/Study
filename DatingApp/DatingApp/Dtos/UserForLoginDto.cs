426cdb3abdcda96269071360d72fa5bdd14f86c5
umesh <umesh.parshad@gmail.com>
2020-01-20 17:45:53 +0530

    Section 3 Completed
    Section 3 Completed- Secrurity

---
+++ DatingApp/DatingApp/Dtos/UserForLoginDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.Dtos
+{
+    public class UserForLoginDto
+    {     
+        [Required]
+        public string username {  get;set;}
+        [Required]
+        [StringLength(8,MinimumLength =4, ErrorMessage ="Your must specify better paassword.")]
+        public string password { get; set; }
+    }
+}
